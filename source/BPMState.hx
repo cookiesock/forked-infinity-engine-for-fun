0746bbf9b5d945f0b1f55634b64d6b94a9318848
Timeless <85826200+timeless13GH@users.noreply.github.com>
2021-12-20 22:27:48 +0000

    do some fucking shit

---
+++ source/BPMState.hx
@@ -0,0 +1,7 @@
+import flixel.FlxState;
+import flixel.util.FlxTimer;
+
+class BPMState extends FlxState
+{
+	public function beatHit(timer:FlxTimer) {}
+}
--- source/TitleScreenState.hx
+++ source/TitleScreenState.hx
@@ -4,19 +4,29 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.util.FlxTimer;
 
-class TitleScreenState extends FlxState
+class TitleScreenState extends BPMState
 {
+	var logo:FlxSprite;
+
 	override public function create():Void
 	{
-		var logo = new FlxSprite();
-		logo.frames = FlxAtlasFrames.fromSparrow("assets/images/logoBumpin.png", "assets/images/logoBumpin.xml");
-		logo.animation.addByPrefix('idle', 'logo bumpin', 24, true);
+		// var BPM = new BPMHandle(102, new TitleScreenState()); fix later lmao
+		logo.frames = FlxAtlasFrames.fromSparrow("assets/images/logoBumpin.png", "assets/images/Start_Screen_Assets.xml");
+		// logo.frames = FlxAtlasFrames.fromSparrow("assets/images/logoZon.png","assets/images/logoZon.xml"); // replace the other one with this for mine :) -- ZonianDX
+		logo.animation.addByPrefix('idle', 'logo bumpin', 24, false);
+		// logo.setGraphicSize(Std.int(logo.width - logo.width * 0.25));
+		// logo.animation.addByPrefix('idle', 'logo bumping lma', 24, true);
+		logo = new FlxSprite();
+		logo.screenCenter();
 		logo.animation.play('idle');
 		add(logo);
 		var pressAccept = new FlxSprite();
 		pressAccept.frames = FlxAtlasFrames.fromSparrow("assets/images/titleEnter.png", "assets/images/titleEnter.xml");
 		pressAccept.animation.addByPrefix('idle', 'Press Enter to Begin', 24, true);
+		pressAccept.screenCenter(X);
+		pressAccept.y = FlxG.height - 140;
 		pressAccept.animation.addByPrefix('pressed', 'ENTER PRESSED', 24, true);
 		pressAccept.animation.play('idle');
 		add(pressAccept);
@@ -26,5 +36,6 @@ class TitleScreenState extends FlxState
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		if (FlxG.keys.pressed.ENTER || FlxG.keys.pressed.SPACE) {}
 	}
 }
