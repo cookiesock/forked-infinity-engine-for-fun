14f1fee66e371afc233d941ebb73877d65e7f6f0
swordcube <49110074+swordcube@users.noreply.github.com>
2021-12-20 16:39:27 -0600

    do very stupid shit
    get rid of the god Damn haxeflixel startup screen and add paths.hx to make my life easier, you don't NEED to use it guys

--- source/TitleScreenState.hx
+++ source/TitleScreenState.hx
@@ -7,24 +7,35 @@ import flixel.graphics.frames.FlxAtlasFrames;
 
 class TitleScreenState extends FlxState
 {
+	public var logo:FlxSprite = new FlxSprite();
+	public var pressAccept = new FlxSprite();
+	
 	override public function create():Void
 	{
-		var logo = new FlxSprite();
-		logo.frames = FlxAtlasFrames.fromSparrow("assets/images/logoBumpin.png", "assets/images/logoBumpin.xml");
+		logo.frames = Paths.getSparrowAtlas('logoBumpin');
 		logo.animation.addByPrefix('idle', 'logo bumpin', 24, true);
 		logo.animation.play('idle');
+		logo.screenCenter(X);
+		logo.y = 55;
 		add(logo);
-		var pressAccept = new FlxSprite();
-		pressAccept.frames = FlxAtlasFrames.fromSparrow("assets/images/titleEnter.png", "assets/images/titleEnter.xml");
+		
+		pressAccept.frames = Paths.getSparrowAtlas('titleEnter');
 		pressAccept.animation.addByPrefix('idle', 'Press Enter to Begin', 24, true);
 		pressAccept.animation.addByPrefix('pressed', 'ENTER PRESSED', 24, true);
 		pressAccept.animation.play('idle');
+		pressAccept.screenCenter(X);
+		pressAccept.y = FlxG.height - 145;
 		add(pressAccept);
+		
 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
+		if(FlxG.keys.justPressed.ENTER)
+		{
+			FlxG.switchState(new PlayState());
+		}
 		super.update(elapsed);
 	}
 }
