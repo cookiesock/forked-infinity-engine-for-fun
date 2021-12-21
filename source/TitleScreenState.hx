ba0dcc4daa69f425a1d5f3e213f09cbbae93dd6f
Raf <rafacgiacom@gmail.com>
2021-12-20 23:53:30 -0300

    BRO DO YOU KNOW WHAT "FROM SCRATCH" MEANS???

--- source/TitleScreenState.hx
+++ source/TitleScreenState.hx
@@ -3,21 +3,13 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.input.keyboard.FlxKey;
-import flixel.addons.display.FlxGridOverlay;
-import flixel.addons.transition.FlxTransitionSprite.GraphicTransTileDiamond;
-import flixel.addons.transition.FlxTransitionableState;
-import flixel.addons.transition.TransitionData;
-import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.group.FlxGroup;
 import flixel.input.gamepad.FlxGamepad;
-import flixel.math.FlxPoint;
-import flixel.math.FlxRect;
+import flixel.input.keyboard.FlxKey;
+import flixel.math.FlxMath;
 import flixel.system.FlxSound;
-import flixel.system.ui.FlxSoundTray;
 import flixel.text.FlxText;
-import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
@@ -26,35 +18,28 @@ import openfl.Assets;
 
 class TitleScreenState extends FlxState
 {
-	public var logo:FlxSprite = new FlxSprite();
-	public var pressAccept:FlxSprite = new FlxSprite();
-	
+	var logo:FlxSprite;
+	var gf:FlxSprite;
+	var pressAccept:FlxSprite;
+
 	override public function create():Void
 	{
-		// transition shit!!! put this in other states that are like menus and shit!!!
-		var diamond:FlxGraphic = FlxGraphic.fromClass(GraphicTransTileDiamond);
-		diamond.persist = true;
-		diamond.destroyOnNoUse = false;
-		
-		FlxTransitionableState.defaultTransIn = new TransitionData(FADE, FlxColor.BLACK, 1, new FlxPoint(0, -1), {asset: diamond, width: 32, height: 32},
-			new FlxRect(-200, -200, FlxG.width * 1.4, FlxG.height * 1.4));
-		FlxTransitionableState.defaultTransOut = new TransitionData(FADE, FlxColor.BLACK, 0.7, new FlxPoint(0, 1),
-			{asset: diamond, width: 32, height: 32}, new FlxRect(-200, -200, FlxG.width * 1.4, FlxG.height * 1.4));
-
-		/*transIn = FlxTransitionableState.defaultTransIn;
-		transOut = FlxTransitionableState.defaultTransOut;*/
-			
-		// menu music yaya
 		FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
 		FlxG.sound.music.fadeIn(4, 0, 0.7);
-		
+
+		logo = new FlxSprite(25, 1000);
 		logo.frames = Paths.getSparrowAtlas('logoBumpin');
 		logo.animation.addByPrefix('idle', 'logo bumpin', 24, true);
 		logo.animation.play('idle');
-		logo.screenCenter(X);
-		logo.y = 55;
 		add(logo);
-		
+
+		gf = new FlxSprite(25, 1000);
+		gf.frames = Paths.getSparrowAtlas('logoBumpin');
+		gf.animation.addByPrefix('idle', 'logo bumpin', 24, true);
+		gf.animation.play('idle');
+		add(gf);
+
+		pressAccept = new FlxSprite();
 		pressAccept.frames = Paths.getSparrowAtlas('titleEnter');
 		pressAccept.animation.addByPrefix('idle', 'Press Enter to Begin', 24, true);
 		pressAccept.animation.addByPrefix('pressed', 'ENTER PRESSED', 24, true);
@@ -62,18 +47,18 @@ class TitleScreenState extends FlxState
 		pressAccept.screenCenter(X);
 		pressAccept.y = FlxG.height - 145;
 		add(pressAccept);
-		
+
 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
-		if(FlxG.keys.justPressed.ENTER)
+		if (FlxG.keys.justPressed.ENTER)
 		{
-			FlxTransitionableState.skipNextTransIn = false;
-			FlxTransitionableState.skipNextTransOut = false;
 			FlxG.switchState(new PlayState());
 		}
+
+		logo.y = FlxMath.lerp(logo.y, 150, Math.max(0, Math.min(1, elapsed * 3)));
 		super.update(elapsed);
 	}
 }
